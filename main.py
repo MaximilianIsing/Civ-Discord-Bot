b51d03ae80e19bd979b22847a6319a96c9f59203
MaximilianIsingCGPS <mising26@cgps.org>
2025-04-16 20:40:05 -0400

    Added admin condition

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ async def vote(ctx, admin = False):
         if len(playerIDs) == 0:
             await ctx.send(f"<#{1351989426951295056}> is empty")
             return
-        if len(playerIDs) == 1:
+        if len(playerIDs) == 1 and not admin:
             await ctx.send(
                 f"<#{1351989426951295056}> has an insufficient amount of players (2 minimum)"
             )
